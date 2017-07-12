e71f2f77a06da29c22b917006ed695d015aa878b
Daniel Hernandes <badah@badah.com.br>
2017-07-12 15:04:41 -0300

    Add shortcode attributes

--- Components/Shortcode.php
+++ Components/Shortcode.php
@@ -6,56 +6,60 @@ use StudioVisual\Support\Contracts\TemplateInterface;
 
 class Shortcode implements TemplateInterface
 {
-    protected $name;
-    protected $data;
-    protected $path;
+	protected $name;
+	protected $data;
+	protected $path;
+	protected $atts;
 
-    public function __construct($name, $data, $path)
-    {
-        $this->name = $name;
-        $this->data = $data;
-        $this->path = $path;
-        $this->register();
-    }
+	public function __construct( $name, $data, $path, $atts ) {
+		$this->name = $name;
+		$this->data = $data;
+		$this->path = $path;
+		$this->atts = $atts;
+		$this->register();
+	}
 
-    public function register()
-    {
-        add_shortcode($this->name, array($this, 'build'));
-    }
+	public function register() {
+		add_shortcode( $this->name, array( $this, 'build' ) );
+	}
 
-    public function getTemplatePath()
-    {
-        return $this->path . '/' . $this->name . '.php';
-    }
+	public function getTemplatePath() {
+		return $this->path . '/' . $this->name . '.php';
+	}
 
-    public function build($atts, $content = null)
-    {
-        foreach ( $this->get_data() as $key => $value ) {
-            $$key = $value;
-        }
+	public function build( $atts, $content = null ) {
 
-        ob_start();
-        include $this->getTemplatePath();
-        $html = ob_get_contents();
-        ob_end_clean();
+		$atts = shortcode_atts( $this->atts, $atts, $this->name );
 
-        return $html;
-    }
+		foreach ( $atts as $attr_name => $attr_value ) {
+			$$attr_name = $attr_value;
+		}
 
-    public function render()
-    {
-        echo do_shortcode("[{$this->name}]");
-    }
+		foreach ( $this->get_data() as $key => $value ) {
+			$$key = $value;
+		}
 
-    public function print_out($data ) {
-        $this->data = $data;
-    }
+		ob_start();
+		include $this->getTemplatePath();
+		$html = ob_get_contents();
+		ob_end_clean();
 
-    protected function get_data() {
-        return $this->data;
-    }
+		return $html;
+	}
 
-    public function is_present() {
-        // Detecta se o shortcode está presenta na página.
-    }
+	public function render() {
+		echo do_shortcode( "[{$this->name}]" );
+	}
+
+	public function print_out( $data ) {
+		$this->data = $data;
+	}
+
+	protected function get_data() {
+		return $this->data;
+	}
+
+	public function the_attr( $attr ) {
+		echo esc_attr__( $attr );
+	}
 }
