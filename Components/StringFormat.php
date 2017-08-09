b9f88d8f779205e1ec073f1b5c6c25a8ca7a0b12
Daniel <badah@badah.com.br>
2017-08-09 15:10:04 -0300

    Add price and number formatting helpers

--- Components/StringFormat.php
+++ Components/StringFormat.php
@@ -8,4 +8,12 @@ class StringFormat {
 		$regex = '/(?<=\d)(?=[A-Za-z])|(?<=[A-Za-z])(?=\d)|(?<=[a-z])(?=[A-Z])/';
 	    return strtolower( preg_replace( $regex, $us, $string ) );
 	}
+
+	public static function price( $string ) {
+		return number_format( (string) $string, 2, ',', '.' );
+	}
+
+	public static function only_numbers( $string ) {
+		return preg_replace( '/[^0-9]/', '', $string );
+	}
 }
