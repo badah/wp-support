4a420ac9bbd0ad18cc3078d9ca2f39bb9b36e36c
Daniel <badah@badah.com.br>
2017-08-17 18:03:50 -0300

    Add value formatting method

--- Components/Collection.php
+++ Components/Collection.php
@@ -9,10 +9,23 @@ namespace StudioVisual\Support\Components;
  */
 class Collection
 {
-	public static function order_by( $field, $array ) {
+	public static function order_by( $field, &$array ) {
 		usort( $array, function( $a, $b ) use ( $field, $array ) {
 			return $a[$field] - $b[$field];
 		});
-		return $array;
+	}
+
+	/**
+	 * @param $array
+	 * @param $callback
+	 * @param $field
+	 * @todo verify if callback is callable
+	 */
+	public static function format( &$array, $callback, $field ) {
+		array_walk_recursive( $array, function( &$input, $key, $field ) use ( $callback ) {
+			if ( $key === $field ) {
+				$input = call_user_func( $callback, $input );
+			}
+		}, $field );
 	}
 }
