9b1f42dd6b8c394e13d7fd9d2eeba1352396de29
Daniel <badah@badah.com.br>
2018-06-29 15:55:55 -0300

    Fix method definition

--- Helpers/class-word.php
+++ Helpers/class-word.php
@@ -4,16 +4,17 @@ namespace Badah\WpSupport\Helpers;
 
 class Word {
 
-	static public function to_snake_case( $string ) {
+	public static function to_snake_case( $string ) {
 		return str_replace( '-', '_', $string );
 	}
 
 	/**
 	 * @param $string
 	 * @return string
+	 *
 	 * @todo Cover all possible situations. E.g.: `foo`, `Foo Bar`,`['foo-bar', 'barFoo']`, ``, etc.
 	 */
-	static function to_camel_case( $string ) {
+	public static function to_camel_case( $string ) {
 		$pieces = explode( '-', $string );
 		$converted = [];
 
