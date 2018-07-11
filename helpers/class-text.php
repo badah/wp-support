62de57775dc493849946c17f350330d437bd7078
Daniel <badah@badah.com.br>
2018-07-11 14:52:07 -0300

    Improve docs

--- helpers/class-text.php
+++ helpers/class-text.php
@@ -4,15 +4,23 @@ namespace Badah\WpSupport\Helpers;
 
 class Text {
 
+	/**
+	 * Converts text to snake case format
+	 *
+	 * @param  string $string   Text to be converted.
+	 * @return string           Converted text.
+	 */
 	public static function to_snake_case( $string ) {
 		return str_replace( '-', '_', $string );
 	}
 
 	/**
-	 * @param $string
-	 * @return string
+	 * Converts text to camel case format
 	 *
-	 * @todo Cover all possible situations. E.g.: `foo`, `Foo Bar`,`['foo-bar', 'barFoo']`, ``, etc.
+	 * @param $string   Text to be converted.
+	 * @return string   Converted text.
+	 *
+	 * @todo Cover more possible situations. E.g.: `foo`, `Foo Bar`,`['foo-bar', 'barFoo']`, ``, etc.
 	 */
 	public static function to_camel_case( $string ) {
 		$pieces = explode( '-', $string );
